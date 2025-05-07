9ff9c5dd35149094c1e7a431341cc806259ec93a
Minos Chatzidakis <mchatzi@users.noreply.github.com>
2025-05-07 15:39:32 +0200

    Update install-mirrorr.sh

--- install-mirrorr.sh
+++ install-mirrorr.sh
@@ -8,8 +8,19 @@ cat <<"EOF"
 
 EOF
 echo -e "Loading..."
+echo -e "Checking and installing dependencies..."
+
+if command -v python3 >/dev/null 2>&1; then
+    if dpkg --compare-versions "$(python3 -V 2>&1 | cut -d' ' -f2)" lt 3.11; then
+        echo "Required Python version is 3.11 or higher, please upgrade!"
+    fi
+else
+    echo "Python 3 is not installed. Installing..."
+    apt install python3 -y
+fi
 
 
+IP=$(ip a s dev eth0 | awk '/inet / {print $2}' | cut -d/ -f1)
 
 #apt install python -y
 #apt install flask -y
