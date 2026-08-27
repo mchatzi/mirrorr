106c8c134684dfc5e19682640019c94380ec2dcf
mchatzi <mchatzi@users.noreply.github.com>
2026-08-27 22:21:11 +0200

    Make updater failsafe

--- install/updaters/v0.6.0-alpha-updater.sh
+++ install/updaters/v0.6.0-alpha-updater.sh
@@ -9,16 +9,22 @@ import yaml
 conf_file_path = '$INSTALLATION_PATH/data/conf.yaml'
 settings = {}
 
-with open(conf_file_path, 'r') as f:
-    settings = yaml.safe_load(f) or {}
+try:
+    with open(conf_file_path, 'r') as f:
+        settings = yaml.safe_load(f) or {}
 
-for field in ['scheduler_cycle_s', 'ui_refresher_s', 'log_retention_count', 'remote_ssh_port']:
-    if field in settings and settings[field] is not None:
-        settings[field] = int(settings[field])
-        print(f'Converted field {field} to integer')
+    for field in ['scheduler_cycle_s', 'ui_refresher_s', 'log_retention_count', 'remote_ssh_port']:
+        if field in settings and settings[field] is not None:
+            try:
+                settings[field] = int(settings[field])
+                print(f'Converted field {field} to integer')
+            except Exception as ee:
+                print(f'Error converting {field}')
 
-with open(conf_file_path, 'w') as f:
-    yaml.dump(settings, stream=f, sort_keys=False)
+    with open(conf_file_path, 'w') as f:
+        yaml.dump(settings, stream=f, sort_keys=False)
+except Exception as e:
+    print('Error while running updater')
 "
 }
 
